69966061274dced57afa82d1f23e4502255bb45c
MariaaPcsa <marialujivan@gmail.com>
2024-04-14 20:56:26 -0300

    afetuado atualização em css,html,js

---
+++ formulario/scrips.js
@@ -0,0 +1,24 @@
+
+const input = document.getElementById("taskInput");
+const ul = document.getElementById("taskList");
+
+function addTask() {
+  if (input.value !== "") {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    //Adiciona um botão para remover a tarefa
+    const removerBotao = document.createElement("button");
+    removerBotao.textContent = "Remover";
+    removerBotao.onclick = function () {
+      ul.removeChild(li);
+    };
+
+    li.appendChild(removerBotao);
+    ul.appendChild(li);
+
+    input.value = "";
+  } else {
+    alert("Por favor, insira uma tarefa.");
+  }
+}
