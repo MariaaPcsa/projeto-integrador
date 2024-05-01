cb397698fd19480f1c1d36e3a4b6bf5f83209556
Andressa Gomes <andrefaele@hotmail.com>
2024-05-01 00:34:30 -0300

    adcionando carrosel

--- js/scrips.js
+++ js/scrips.js
@@ -1,22 +1,14 @@
-///
-///
+let count = 1;
+document.getElementById("radio1").checked =true;
+setInterval(function(){
+    nextImage();
+},5000)
 
-function mudarCor(elemento, cor) {
-    elemento.style.color = cor;
-}
-/**onclick=" alert('botão clicado')" */
-function clicarBotao() {
-    alert('Botão foi clidado')
-}
-/**onfocus="this.value=('campo em foco')" onblur="this.value=('') */
-
-function emFoco(elemento) {
-    elemento.value = 'Campo em foco'
-}
-function semFoco(elemento) {
-    elemento.value = ''
-}
-/**função para altera a imagem */
-function mudarImagemFacil(imagem, novoSrc){
-    imagem.src=novoSrc;
+function nextImage(){
+    count++;
+    if (count>4)
+    {
+        count=1;
+    }
+    document.getElementById("radio"+count).checked =true;
 }
