e579bf80538d4d7b48ff9fe8239a8bfa0a5509ab
MariaaPcsa <marialujivan@gmail.com>
2024-05-02 23:06:37 -0300

    incluinda incones no formulario , arrumando campo senha

--- js/depoimento.js
+++ js/depoimento.js
@@ -1,14 +1,14 @@
-
-
 const input = document.getElementById("taskInput");
+const checkbox = document.getElementById("checkbox");
 const ul = document.getElementById("taskList");
-const forme = document.getElementById("cadastro-form")
+const forme = document.getElementById("cadastro-form");
+
 function addTask() {
-  if (input.value !== "") {
+  if (input.value !== "" && checkbox.checked) {
     const li = document.createElement("li");
     li.textContent = input.value;
 
-    //Adiciona um botão para remover a tarefa
+    // Adiciona um botão para remover a tarefa
     const removerBotao = document.createElement("button");
     removerBotao.textContent = "Remover";
     removerBotao.onclick = function () {
@@ -19,10 +19,13 @@ function addTask() {
     ul.appendChild(li);
 
     input.value = "";
-  } else {
+  } else if (input.value == "" && checkbox.checked) {
     alert("Por favor, insira uma tarefa.");
+  } else if (!checkbox.checked) {
+    alert("Para enviar um comentário é necessário aceitar que a mensagem seja exibida.");
   }
 }
+
 function enviarMensagem() {
   alert("Dados enviados!");
 }
